9efafa7888f6525ab799d8cb00434011d0ddab59
santiago <spereira98@misena.edu.co>
2021-08-19 23:34:54 -0500

    Codigo
    Desarrollo del codigo

---
+++ codigo.js
@@ -0,0 +1,11 @@
+
+let nombres, apellido1, apellido2;
+nombres = "Santiago";
+apellido1 = "Pereira";
+apellido2 = "Gonzalez";
+alert(nombres);
+alert(apellido1);
+alert(apellido2);
+
+let respuesta = prompt("como te encuentras");
+alert(respuesta + " ah pa saber.");
