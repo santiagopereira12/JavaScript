b4470a62437a44c4a33f37a6704b9df15900392d
santiago <spereira98@misena.edu.co>
2021-09-06 00:45:54 -0500

    Se agregaron ediciones

--- cofla.js
+++ cofla.js
@@ -11,54 +11,71 @@ let dineroCristian = prompt("¿cuanto dinero posees Cristian?");
 let dineroSantiago = prompt("¿cuanto dinero posees Santiago?");
 
 if (dineroCofla >= 600 && dineroCofla < 1000){
-    alert("Cofla comprate el helado de " + helado1);
+    alert("Cofla comprate el " + helado1);
+    dineroCofla -= 600;
 }else if(dineroCofla >= 1000 && dineroCofla < 1600){
-    alert("Cofla comprate el helado de " + helado2);
+    alert("Cofla comprate el " + helado2);
+    dineroCofla -= 1000;
 }else if(dineroCofla >= 1600 && dineroCofla < 1700){
-    alert("Cofla comprate el helado de " + helado3);
+    alert("Cofla comprate el " + helado3);
+    dineroCofla -= 1600;
 }else if(dineroCofla >= 1700 && dineroCofla < 1800){
-    alert("Cofla comprate el helado de " + helado4);
+    alert("Cofla comprate el " + helado4);
+    dineroCofla -= 1700;
 }else if(dineroCofla >= 1800 && dineroCofla < 2900){
-    alert("Cofla comprate el helado de " + helado5);
+    alert("Cofla comprate el " + helado5);
+    dineroCofla -= 1800;
 }else if(dineroCofla >= 2900){
-    alert("Cofla comprate el helado de " + helado6 + " o tambien el " + helado7);
+    alert("Cofla comprate el " + helado6 + " o tambien el " + helado7);
+    dineroCofla -= 2900;
 }else{
     alert("veo pirobo levantese las lukas completas");
 }
 
 if (dineroCristian >= 600 && dineroCristian < 1000){
-    alert("Cristian comprate el helado de " + helado1);
+    alert("Cristian comprate el " + helado1);
+    dineroCristian -= 600;
 }else if(dineroCristian >= 1000 && dineroCristian < 1600){
-    alert("Cristian comprate el helado de " + helado2);
+    alert("Cristian comprate el " + helado2);
+    dineroCristian -= 1000;
 }else if(dineroCristian >= 1600 && dineroCristian < 1700){
-    alert("Cristian comprate el helado de " + helado3);
+    alert("Cristian comprate el " + helado3);
+    dineroCristian -= 1600;
 }else if(dineroCristian >= 1700 && dineroCristian < 1800){
-    alert("Cristian comprate el helado de " + helado4);
+    alert("Cristian comprate el " + helado4);
+    dineroCristian -= 1700;
 }else if(dineroCristian >= 1800 && dineroCristian < 2900){
-    alert("Cristian comprate el helado de " + helado5);
+    alert("Cristian comprate el " + helado5);
+    dineroCristian -= 1800;
 }else if(dineroCristian >= 2900){
-    alert("Cristian comprate el helado de " + helado6 + " o tambien el " + helado7);
+    alert("Cristian comprate el " + helado6 + " o tambien el " + helado7);
+    dineroCristian -= 2900;
 }else{
     alert("veo marico levantese las lukas completas");
 }
 
 if (dineroSantiago >= 600 && dineroSantiago < 1000){
     alert("Santiago comprate el " + helado1);
+    dineroSantiago -= 600;
 }else if(dineroSantiago >= 1000 && dineroSantiago < 1600){
     alert("Santiago comprate el " + helado2);
+    dineroSantiago -= 1000;
 }else if(dineroSantiago >= 1600 && dineroSantiago < 1700){
     alert("Santiago comprate el " + helado3);
+    dineroSantiago -= 1600;
 }else if(dineroSantiago >= 1700 && dineroSantiago < 1800){
     alert("Santiago comprate el " + helado4);
+    dineroSantiago -= 1700;
 }else if(dineroSantiago >= 1800 && dineroSantiago < 2900){
     alert("Santiago comprate el " + helado5);
+    dineroSantiago -= 1800;
 }else if(dineroSantiago >= 2900){
     alert("Santiago comprate el " + helado6 + " o tambien el " + helado7);
+    dineroSantiago -= 2900;
 }else{
     alert("veo rey no te puedo fiar");
 }
 
-dineroCofla -= 1000;
-dineroSantiago -= 2900;
 alert(`Cofla te sobran ${dineroCofla} pesos`);
+alert(`Cristian te sobrtan ${dineroCristian} pesos`);
 alert(`Santiago te sobran ${dineroSantiago} pesos`);
