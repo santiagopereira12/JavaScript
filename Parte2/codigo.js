9dfbb407db6164c5c12070ffa5c2e505b5b832ce
santiagopereira12 <spereira98@misena.edu.co>
2021-10-11 15:08:14 -0500

    Realización de arrays

--- Parte2/codigo.js
+++ Parte2/codigo.js
@@ -1,3 +1,25 @@
 
-let Bienvenida = "Seguimos con el curso de javascript,  ";
+let Bienvenida = "Seguimos con el curso de javascript. <br>";
 document.write(Bienvenida);
+
+let comida = ["hambirguesa <br>", "empanada <br>", "pizza <br>", "salchipapa <br>", "perrrocaliente <br>"];
+document.write(comida[2]);
+
+let computador = {
+    marca: "Janus",
+    procesador: "Intel core i3",
+    ram: "12GB",
+    espacio: "500GB"
+};
+
+let marca = computador["marca"];
+let procesador = computador["procesador"];
+let ram = computador["ram"];
+let espacio = computador["espacio"];
+
+let referencia = `La marca del dispositivo es: <b>${marca}</b> <br>
+                  El procesador es: <b>${procesador}</b> <br>
+                  La ram es de: <b>${ram}</b> <br>
+                  La memoria es de: <b>${espacio}</b> <br>`;
+
+document.write(referencia);
