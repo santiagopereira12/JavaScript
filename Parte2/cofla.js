c02592971836e75c590215ee57b5dffc2a8ce1f0
santiagopereira12 <spereira98@misena.edu.co>
2021-11-02 11:46:13 -0500

    Inicio de solución de proyecto

---
+++ Parte2/cofla.js
@@ -0,0 +1,10 @@
+function registerEdad(){
+    while(edad >= 18){
+        document.write("ingresa a la farriusqui");
+        if(edad < 18){
+            break;
+            document.write("<b>Pa' tu casa a ver pocoyo</b>");
+        }
+    }
+}
+registerEdad = prompt("ingrese su edad");
