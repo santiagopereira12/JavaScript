d8112120d8295428aa1e7d00a837dbafdc1567cb
santiagopereira12 <spereira98@misena.edu.co>
2021-11-13 14:41:39 -0500

    Caracteristicas y conceptos de POO

--- Parte3/codigo.js
+++ Parte3/codigo.js
@@ -3,7 +3,7 @@ let Bienvenida = "<b style= 'color: red'> Segimos con el curso de Javascript p3<
 document.write(Bienvenida);
 
 //Creación de clase para trabajar dentro.
-class animal{
+class Animal{
     constructor(especie,edad,color){
         this.especie = especie;
         this.edad = edad;
@@ -13,16 +13,40 @@ class animal{
     verInformacion(){
         document.write(this.info +"<br>");
     }
+    /* ladrar(){
+        if(this.especie == "Perro" || this.especie == "perro"){
+            document.write("¡waoh! <br>");
+        }else{
+            document.write("No puede ladrar ya que es un " +this.especie +"<br>");
+        }
+    } */
+}
+//Herencia
+class Perro extends Animal{
+    constructor(especie, edad,color,raza){
+        super(especie, edad,color)
+        this.raza = null;
+    }
+    static ladrar(){
+        alert("¡WAOH!");
+    }
+    set setRaza(newName){
+        this.raza = newName;
+    }
+    get getRaza(){
+        return this.raza;
+    }
 }
 //Instanciar la clase.
-let gato = new animal("Gato",3,"amarillo");
-let perro = new animal("Perro",5,"negro");
-let conejo = new animal("Conejo",1,"blanco");
+const gato = new Animal("Gato",3,"amarillo");
+const perro = new Perro("Perro",5,"negro","pitbul");
+const conejo = new Animal("Conejo",1,"blanco");
 /* document.write(gato.info +"<br>");
 document.write(perro.info +"<br>");
 document.write(conejo.info +"<br>"); */
 gato.verInformacion();
-perro.verInformacion();
+perro.setRaza = "Cristian";
+document.write(perro.getRaza);
 conejo.verInformacion();
 document.write("<br>");
 document.write("<b>hasta la proxima malparido hijueputa</b>");
