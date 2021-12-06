fdbb6a75176d74ef539e5f0dfbee124e6b097887
santiagopereira12 <spereira98@misena.edu.co>
2021-12-06 14:44:30 -0500

    Metodos de Atributo de un elemento

--- Parte6/codigo.js
+++ Parte6/codigo.js
@@ -1,3 +1,13 @@
 //Inicio de curso p6.
 let Bienvenida = "<b style= 'color:red'>Seguimos con el curso de Javascript p6</b><br>";
 document.write(Bienvenida);
+
+//Metodos de seleccion de elementos.
+parrafo = document.getElementById("parrafo");
+document.write(parrafo);
+
+seleccionar = document.getElementsByTagName("p");
+document.write(seleccionar);
+
+clases = document.querySelector(".prueba");
+document.write(clases);
