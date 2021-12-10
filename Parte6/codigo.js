d8a948c79224d0bb63d260b447145b739a1a7efc
santiagopereira12 <spereira98@misena.edu.co>
2021-12-10 11:37:49 -0500

    Atributos Globales

--- Parte6/codigo.js
+++ Parte6/codigo.js
@@ -1,9 +1,9 @@
 //Inicio de curso p6.
-let Bienvenida = "<b style= 'color:red'>Seguimos con el curso de Javascript p6</b><br>";
+let Bienvenida = "<br><b style= 'color:red'>Seguimos con el curso de Javascript p6</b><br>";
 document.write(Bienvenida);
 
 //Metodos de seleccion de elementos.
-parrafo = document.getElementById("parrafo");
+parrafo = document.getElementById("<br>parrafo");
 document.write(parrafo);
 
 seleccionar = document.getElementsByTagName("p");
@@ -11,3 +11,12 @@ document.write(seleccionar);
 
 clases = document.querySelector(".prueba");
 document.write(clases);
+
+const rangoEdad = document.querySelector(".rangoEdad");
+/*cambiar el valor.
+rangoEdad.setAttribute("type","color");*/
+valorAtribut = rangoEdad.getAttribute("type");
+document.write(valorAtribut);
+
+//Atributos globales.
+const prueba = document.querySelector(".prueba");
