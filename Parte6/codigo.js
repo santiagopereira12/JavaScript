99ee4f923ec3bc9292e0f1975311230cfbf43a51
santiagopereira12 <spereira98@misena.edu.co>
2021-12-12 14:32:41 -0500

    Propiedad Style.

--- Parte6/codigo.js
+++ Parte6/codigo.js
@@ -27,3 +27,17 @@ const seleccion = document.querySelector(".seleccion");
 seleccion.setAttribute("tabindex","0");
 const eva = document.querySelector(".eva");
 eva.setAttribute("title","sapo");
+
+//Atributos de input.
+const input = document.querySelector(".inputNormal");
+document.write("<br>este puto<br>" +input.className);
+document.write(input.value +"<br>");
+document.write(input.type="number");
+const imagen = document.querySelector(".imagen");
+imagen.accept = "imagen/jpg";
+const minimo = document.querySelector(".minimo");
+minimo.placeholder = "Escribe putito";
+minimo.required = " ";
+minimo.setAttribute("minLength","5");
+
+//propiedad Style.
