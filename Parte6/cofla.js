da67caf8602c386f9ffeb66eaf0551250b702d8a
santiagopereira12 <spereira98@misena.edu.co>
2021-12-27 14:04:39 -0500

    Solución de problemas Cofla.

--- Parte6/cofla.js
+++ Parte6/cofla.js
@@ -1,8 +1,8 @@
 const contenedor = document.querySelector(".flex-container");
-let contador = 0;
+
+document.querySelector(".send-button").value = "COMPRAR";
 
 function crearLlave(nombre,modelo,precio){
-    contador++;
     img = "<img class='llave-img' src='llave.png'>";
     nombre = `<h2>${nombre}</h2>`;
     modelo = `<h3>${modelo}</h3>`;
@@ -17,6 +17,9 @@ for (var i = 1; i <= 20; i++){
     let precioRandom = Math.round(Math.random()*10000+300);
     let llave = crearLlave(`llave ${i}`, `modelo ${modeloRandom}`,precioRandom);
     let div = document.createElement("DIV");
+    div.addEventListener("click",()=>{
+        document.querySelector(".key-data").value = modeloRandom;
+    });
     div.tabIndex = i;
     div.classList.add(`item-${i}`,`flex-item`);
     div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
