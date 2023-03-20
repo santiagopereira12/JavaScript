b07f098001075cdcaba16c6546019d1225f4dc9e
santiagopereira12 <spereira98@misena.edu.co>
2023-03-20 14:40:10 -0500

    Inicio Curso javascript parte 7

---
+++ parte7/codigo.js
@@ -0,0 +1,16 @@
+//Inicio de curso p7.
+let Bienvenida = "<br><b style='color:red'>Retomamos el Curso de JavaScript p7</b></br>";
+document.write(Bienvenida);
+
+//open().
+let youtubeurl = "https://youtube.com";
+let ventanabierta = window.open(youtubeurl);
+
+/* close();
+ventanabierta.close();*/
+
+/*closed 
+ventanabierta.closed*/
+
+/*stop() 
+window.stop();*/
